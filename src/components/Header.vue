353d8c0c14af16213ec37125c98114b9c2267e17
Manuel <Manuelasomc@gmail.com>
2020-07-24 23:36:04 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled>Quizz</b-nav-item>
+      <b-nav-item disabled>Counter:</b-nav-item>
+    </b-nav>
+  </div>
+</template>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+  <b-jumbotron>
+    <template v-slot:header>BootstrapVue</template>
+
+    <template v-slot:lead>
+      This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+      featured content or information.
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+      It uses utility classes for typography and spacing to space content out within the larger
+      container.
+    </p>
+
+    <b-button variant="primary" href="#">submit</b-button>
+    <b-button variant="success" href="#">next</b-button>
+  </b-jumbotron>
+</div>
+</template>
